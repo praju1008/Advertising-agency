047be2725d13e4e36688a1275a7af56fd318d001
Prajwal <mathapatipraju1008@gmail.com>
2025-10-30 15:29:02 +0530

    logo update

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -12,7 +12,7 @@ const Testimonials = () => {
           <div className="quote-demo-icon">
             <svg width="36" height="36" viewBox="0 0 24 24" fill="none"><path d="M10 9V15C10 16.1 9.1 17 8 17H4C2.9 17 2 16.1 2 15V11C2 7.69 4.69 5 8 5H9V7H8C5.79 7 4 8.79 4 11H8C9.1 11 10 11.9 10 13V15ZM22 9V15C22 16.1 21.1 17 20 17H16C14.9 17 14 16.1 14 15V11C14 7.69 16.69 5 20 5H21V7H20C17.79 7 16 8.79 16 11H20C21.1 11 22 11.9 22 13V15Z" fill="#fff"/></svg>
           </div>
-          <div className="testimonials-author-demo">Sachin Agarwal</div>
+          <div className="testimonials-author-demo">R N Arakeri</div>
         </div>
         <div className="client-logos-demo">
           <div className="logo-demo-box">
