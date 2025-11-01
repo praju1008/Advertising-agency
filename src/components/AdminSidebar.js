8431b590230792c806a5b03f9d16bec3ac8f5cb2
Prajwal <mathapatipraju1008@gmail.com>
2025-11-01 15:57:19 +0530

    logo update

---
+++ src/components/AdminSidebar.js
@@ -0,0 +1,18 @@
+// import React from "react";
+// import { Link } from "react-router-dom";
+// import "./AdminSidebar.css";
+
+// const AdminSidebar = () => (
+//   <nav className="admin-sidebar">
+//     <h2 className="admin-sidebar-title">Admin Area</h2>
+//     <ul>
+//       <li><Link to="/admin">Dashboard</Link></li>
+//       <li><Link to="/admin/jobs">Jobs</Link></li>
+//       {/* Add more sections below as you expand */}
+//       {/* <li><Link to="/admin/services">Services</Link></li> */}
+//       {/* <li><Link to="/admin/banners">Banners</Link></li> */}
+//     </ul>
+//   </nav>
+// );
+
+// export default AdminSidebar;
